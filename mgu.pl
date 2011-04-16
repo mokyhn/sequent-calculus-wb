2225dc69670fea99ec7dee468dff5a64a36e1a82
mokyhn <mokyhn@c178890b-3065-5e94-d4a0-d2f1641d0758>
2011-04-16 07:56:57 +0000

    corrected mgu calculation

--- mgu.pl
+++ mgu.pl
@@ -6,7 +6,7 @@
 %%
 
 
-:- module(mgu, [mmgu/2, mmgu_com/2, mmgu_ii/2]).
+%:- module(mgu, [mmgu/2, mmgu_com/2]).
 
 :- use_module(subst).
 
@@ -22,93 +22,137 @@ vars([], []).
 vars(const(_C), []).
 vars(var(X), [var(X)]).
 vars(func(_Name, Args), V) :- vars(Args, V).
+vars(eq(T1, T2), V) :-
+    vars(T1, V1),
+	vars(T2, V2),
+	union(V1, V2, V).
 vars([T|Ts], R) :-
 	vars(T,  R1),
 	vars(Ts, R2),
 	union(R1, R2, R).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Function symbols
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+% Mismatch of function symbol names
+mmgu_i(E1, E2) :-
+	member(E, E1),
+	E = eq(func(Name1, _), func(Name2, _)),
+	Name1 \== Name2,
+	E2 = [error],
+	!.
+
+
+mmgu_i(E1, E2) :-
+	member(E, E1),
+	E = eq(func(Name, Args1), func(Name, Args2)),
+    length(Args1, Len1),
+    length(Args2, Len2),
+    Len1 \== Len2,
+	E2 = [error],
+	!.
 
 
 %If E is f(s1, ..., sn) = f(t1, ..., tn) then remove E from E1 and
 %add s1 = t1 ... sn = tn. Return result as E2.
 mmgu_i(E1, E2) :-
 	member(E, E1),
-	(E = eq(func(Name1, Args1), func(Name2, Args2))  % Condition for the rule
-	  ->                                            % Body of the rule
-	(Name1 = Name2 ->
-		(
-		   length(Args1, Len1),
-		   length(Args2, Len2),
-		   Len1 = Len2 ->
-		   (
-			 subtract(E1, [E], Ep),
-			 gen_eqs(Args1, Args2, Eqs),
-			 union(Ep, Eqs, E2)
-		   ) ; E2 = [error_ia] % Mismatch of number of term arguments
-		) ; E2 = [error_ib] % Mismatch of function symbol names
-	) ;
-	E2 = E1	  % ...otherwise behave as the identity function
-	).
-	
+	E = eq(func(Name, Args1), func(Name, Args2)),  % Condition for the rule
+	length(Args1, Len),
+	length(Args2, Len),
+    subtract(E1, [E], Ep),
+	gen_eqs(Args1, Args2, Eqs),
+	union(Ep, Eqs, E2),
+	!.
+
+mmgu_i(E1, E2) :- E2 = E1.	  % ...otherwise behave as the identity function
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Identity x = x
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+
 % If x = x occurs in E1 remove this and return
 % the rest of equations in E2
 mmgu_ii(E1, E2) :-
 	 member(E, E1),
-	 (E = eq(var(X), var(X)) % Condition
-	  ->
-	    subtract(E1, [E], E2);
-	  E2 = E1 % ... otherwise behave as the identity function
-      )	 
-	 . 
+	 E = eq(var(X), var(X)), % Condition
+	 subtract(E1, [E], E2), !.
+	 
+mmgu_ii(E1, E2) :- E1 = E2. % ... otherwise behave as the identity function
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Term t = x
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
 
 % If t=x is in E1 and t is not a variable then replace with x=t and
 % return result in E2
 mmgu_iii(E1, E2) :-
 	 member(E, E1),
-	 (E = eq(T, var(X)),
- 	 not(T=var(_))
-	  ->
-	 (
-	  subtract(E1, [E], Ep),
-	  union(Ep, [eq(var(X), T)], E2)
-	 ) ;
-	 E2 = E1  % ... otherwise behave as the identity function
-	 ). 
+	 E = eq(T, var(X)),
+	 not(T=var(_)),
+     subtract(E1, [E], Ep),
+	 union(Ep, [eq(var(X), T)], E2), !.
+	 
+mmgu_iii(E1, E2) :- E2 = E1.  % ... otherwise behave as the identity function
+	
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Term x = t
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+	 	 	 
 mmgu_iv(E1, E2) :-
 	member(E, E1),
-	(E = eq(var(X), T),
+	E = eq(var(X), T),
 	vars(T, VarsT),
-	not(T=var(X)) -> 
-	(
-		(  not(member(var(X), VarsT)) ->
-		 (
-		  subtract(E1, [E], Ep),
-		  subst(Ep, var(X), T, Epp),
-		  E2 = [E|Epp]
-		 ) ; E2 = [error_iv]
-		)
-	) ;
-	E2 = E1  % ... otherwise behave as the identity function
-	)
-	. 
+	member(var(X), VarsT),
+	E2 = [error], 
+	!.
+			 
+mmgu_iv(E1, E2) :-
+	member(E, E1),
+	E = eq(var(X), T),
+	not(T=var(X)),
+    subtract(E1, [E], Ep),
+	vars(Ep, VEp),
+	member(var(X), VEp),
+	subst(Ep, var(X), T, Epp),
+	E2 = [E|Epp], !.
+	
+mmgu_iv(E1, E2)	:- E2 = E1.  % ... otherwise behave as the identity function
+
+
+
+	
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Composition of functions mmgu_i, ... mmgu_iv
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+	
+mmgu_com([error], [error]).
 
 % A composition of the above functions
-mmgu_com(Es, Er) :-
+mmgu_com(Es, Er) :- 
 	mmgu_i(Es, E1),
 	mmgu_ii(E1, E2),
-	mmgu_iii(E2, E3),
+	mmgu_iii(E2, E3), 
 	mmgu_iv(E3, Er).
 	
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Fixed point computation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
 	
-% Fixpoint computation using mmgu_com
 mmgu(R1, R2) :-
     mmgu_com(R1, Rp),
-	(R1 = Rp
-       -> 
-	    R2 = Rp  ;
-	    mmgu(Rp, R2)
-	).	
-
+	R1 = Rp,
+	R2 = Rp.
+	
+mmgu(R1, R2) :-
+    mmgu_com(R1, Rtmp),
+    mmgu(Rtmp, R2).
 
 	
 
--- test_mgu.pl
+++ test_mgu.pl
@@ -6,8 +6,10 @@
 :- use_module(io).
 
 
+:- p('f(X,Y,Z)=g(A,B,C)', R1), 
+   mmgu([R1], R2).
 
-:- p('X=X', R1), mmgu([R1], R2), print('mmgu: ['), pp(R1), print(']  becomes ['), pp(R2), print(']'), nl, nl.
+%:- p('X=X', R1), mmgu([R1], R2), print('mmgu: ['), pp(R1), print(']  becomes ['), pp(R2), print(']'), nl, nl.
 
 
 h(and(E1,E2), R) :- h(E1, R1),  h(E2, R2), union(R1, R2, R).
@@ -15,3 +17,4 @@ h(T, [T]).
 
 
 :- p('X=Y&Y=Z', R1), h(R1, R2), mmgu(R2, R3), print('mmgu: ['), pp(R2), print(']  becomes ['), pp(R3), print(']'), nl, nl.
+
